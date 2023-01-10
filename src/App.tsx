8e19abbe488bade020bf5a29de8700474295548b
daveclinton <clintondavid46@gmail.com>
2023-01-10 19:52:43 +0300

    feature: Remove color mode switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import * as React from "react";
 import { ChakraProvider, Box, theme } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import Home from "./pages";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box>
-      <ColorModeSwitcher justifySelf="flex-end" display="none" />
-    </Box>
+    <Box></Box>
     <Home />
   </ChakraProvider>
 );
