160cf02837a3c347b8a24cffa1271cb45b89ae8b
daveclinton <clintondavid46@gmail.com>
2023-01-10 16:51:16 +0300

    add navbar and home component

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+type NavbarProps = {};
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return <Box>Hello</Box>;
+};
+
+export default Navbar;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+type HomeProps = {};
+
+const Home: React.FC<HomeProps> = () => {
+  return <Navbar />;
+};
+
+export default Home;
