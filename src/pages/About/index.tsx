736bef2f9a591cbfccff143bcbc2a869a2685b4d
daveclinton <clintondavid46@gmail.com>
2023-01-10 20:20:58 +0300

    feature: Add pages

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+const About = () => {
+  return <Navbar />;
+};
+
+export default About;
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+const Blog = () => {
+  return <Navbar />;
+};
+
+export default Blog;
