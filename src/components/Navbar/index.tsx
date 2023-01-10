0a93b618fd9f52007bf79999c79c9d5f1e2a8e8c
daveclinton <clintondavid46@gmail.com>
2023-01-10 19:53:11 +0300

    fix: Fix eslint conflicts

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Flex, Link } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "../../ColorModeSwitcher";
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
   return (
-    <Flex>
+    <Flex
+      color="#104c68"
+      mt="19px"
+      mr="16px"
+      gap="15px"
+      float="right"
+      w="auto"
+      justify="center"
+      align="center"
+      fontSize="15px"
+    >
       <Link>Blog</Link>
       <Link>About</Link>
+      <ColorModeSwitcher justifySelf="flex-end" />
     </Flex>
   );
 };
