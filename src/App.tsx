0424ca2f05fa505745a0f3c3c008a0306b273737
daveclinton <clintondavid46@gmail.com>
2023-01-10 20:20:34 +0300

    feature: Add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
-import { ChakraProvider, Box, theme } from "@chakra-ui/react";
-import Home from "./pages";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Blog from "./pages/blog";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box></Box>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   </ChakraProvider>
 );
