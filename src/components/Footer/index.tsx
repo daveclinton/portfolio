cfe18a76291160b1a8e870a74bb278255b1ca51e
daveclinton <clintondavid46@gmail.com>
2023-01-16 09:10:57 +0300

    feature: Complete footer design

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,6 +6,8 @@ import {
   Text,
   UnorderedList,
   Icon,
+  Flex,
+  Link,
 } from "@chakra-ui/react";
 import { VscGithub, VscTwitter } from "react-icons/vsc";
 import { AiFillLinkedin } from "react-icons/ai";
@@ -14,24 +16,58 @@ type FooterProps = {};
 
 const Footer: React.FC<FooterProps> = () => {
   return (
-    <Box>
-      <Box>
-        <Heading as="h3">Clinton David</Heading>
-        <Text>
+    <Box bg="#00294d" h="auto" w="100%" pt="10px" color="#fff" mt="40px">
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        flexDir="column"
+        textAlign="center"
+      >
+        <Heading as="h3" fontSize="24px" lineHeight="3rem">
+          Clinton David
+        </Heading>
+        <Text maxW="500px" m="5px auto" lineHeight="28px" fontSize="14px">
           Lorem ipsum is placeholder text commonly used in the graphic, print,
           and publishing
         </Text>
-        <UnorderedList>
-          <ListItem>
-            <Icon as={VscGithub} />
+        <UnorderedList
+          listStyleType="none"
+          display="flex"
+          justifyContent="center"
+        >
+          <ListItem
+            m="0 10px"
+            _hover={{ color: "yellow" }}
+            transition="color .4s ease"
+          >
+            <Link href="https://github.com/daveclinton">
+              <Icon as={VscGithub} />
+            </Link>
           </ListItem>
-          <ListItem>
-            <Icon as={VscTwitter} />
+          <ListItem
+            m="0 10px"
+            _hover={{ color: "yellow" }}
+            transition="color .4s ease"
+          >
+            <Link href="https://twitter.com/daveclintonn">
+              <Icon as={VscTwitter} />
+            </Link>
           </ListItem>
-          <ListItem>
-            <Icon as={AiFillLinkedin} />
+          <ListItem
+            m="0 10px"
+            _hover={{ color: "yellow" }}
+            transition="color .4s ease"
+          >
+            <Link href="https://www.linkedin.com/in/clintondavid46/">
+              <Icon as={AiFillLinkedin} />
+            </Link>
           </ListItem>
         </UnorderedList>
+      </Flex>
+      <Box bg="#fff" w="auto" textAlign="center" color="#00294d">
+        <Text fontSize="14px" letterSpacing="2px" fontWeight={200}>
+          copyright &copy;2023. designed by <span>ME</span>
+        </Text>
       </Box>
     </Box>
   );
