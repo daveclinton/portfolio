5d5bd5278f8105c49c10fe916ad75cb2fe68937a
daveclinton <clintondavid46@gmail.com>
2023-03-20 09:18:58 +0300

    Fix: Add chnges

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import React, { useState} from 'react'
-import About from './components/About/About';
-import Homepage from './components/Homepage/Homepage'
-import Navbar from './components/Navbar/Navbar'
-import Portfolio from './components/Portfolio/Portfolio';
-import Contact from './components/Contact/Contact';
+import React, { useState } from "react";
+import About from "./components/About/About";
+import Homepage from "./components/Homepage/Homepage";
+import Navbar from "./components/Navbar/Navbar";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Contact from "./components/Contact/Contact";
 
 function App() {
-
-  
-  
-
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
 
   const toggleMode = () => {
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#181a1c';
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#181a1c";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
     }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-    }
-  }
+  };
 
   return (
     <>
-        <Navbar brand="Vaibhav" mode={mode} toggleMode={toggleMode} />
-        <Homepage homepageImage="../" mode={mode} />
-        <Portfolio mode={mode}/>
-        <About mode={mode}/>
-        <Contact mode={mode}/>
+      <Navbar brand="David" mode={mode} toggleMode={toggleMode} />
+      <Homepage homepageImage="../" mode={mode} />
+      <Portfolio mode={mode} />
+      <About mode={mode} />
+      <Contact mode={mode} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
