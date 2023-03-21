55e0d94b0dc621363d28a20bdd6f6ee0e3e86327
daveclinton <clintondavid46@gmail.com>
2023-03-21 22:21:49 +0300

    Feat: Add moon icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,47 +1,63 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
+import { FaMoon } from "react-icons/fa";
 
-function Navbar(props) {
+const Navbar = (props) => {
   return (
     <>
-        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} py-3`}>
-      <div className="container">
-        <a href="/" className={"navbar-brand"}>{ props.brand }</a>
+      <nav
+        className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} py-3`}
+      >
+        <div className="container">
+          <a href="/" className={"navbar-brand"}>
+            {props.brand}
+          </a>
 
-        <div
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navmenu"
-        >
-          <span className="navbar-toggler-icon"></span>
+          <div
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navmenu"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </div>
+
+          <div className="collapse navbar-collapse" id="navmenu">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a href="#home" className="nav-link">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#portfolio" className="nav-link">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#about" className="nav-link">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#contact" className="nav-link">
+                  Contact
+                </a>
+              </li>
+            </ul>
+
+            <div
+              className={`form-check form-switch text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
+              <FaMoon onClick={props.toggleMode} cursor="pointer" />
+            </div>
+          </div>
         </div>
-
-        <div className="collapse navbar-collapse" id="navmenu">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a href="#home" className="nav-link">Home</a>
-            </li>
-            <li className="nav-item">
-              <a href="#portfolio" className="nav-link">Portfolio</a>
-            </li>
-            <li className="nav-item">
-              <a href="#about" className="nav-link">About</a>
-            </li>
-            <li className="nav-item">
-              <a href="#contact" className="nav-link">Contact</a>
-            </li>
-          </ul>
-
-          <div className={`form-check form-switch text-${props.mode ==='light'? 'dark': 'light'}`}>
-          <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-          <label className="form-check-label" htmlFor="flexSwitchCheckDefault"> Dark Mode </label>
-         </div>
-        </div>
-      </div>
-    </nav>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
